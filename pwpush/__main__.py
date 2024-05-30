349c71aaa4bf2afc9c84b38501e4d3bfde8e2288
Steffen Fredriksen <Hellowlol@users.noreply.github.com>
2024-05-30 10:32:30 +0200

    Passphrase (#870)
    * First pass on passphrase
    
    * Change to option and add passphrase to payload
    
    ---------
    
    Co-authored-by: Steffen Fredriksen <steffen.fredriksen@orbyt.tech>
    Co-authored-by: Peter Giacomo Lombardo <pglombardo@hey.com>

--- pwpush/__main__.py
+++ pwpush/__main__.py
@@ -121,6 +121,7 @@ def push(
     payload: str = typer.Argument(
         "",
     ),
+    passphrase: str = typer.Option(None, help="Add a passphrase to access the secret"),
 ) -> None:
     """
     Push a new password, secret note or text.
@@ -156,6 +157,9 @@ def push(
         data["password"]["retrieval_step"] = views
     elif user_config["expiration"]["retrieval_step"] != "Not Set":
         data["password"]["retrieval_step"] = user_config["expiration"]["retrieval_step"]
+
+    if passphrase:
+        data["password"]["passphrase"] = passphrase
 
     response = make_request("POST", path, post_data=data)
 
